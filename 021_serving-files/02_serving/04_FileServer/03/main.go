56f554c1ee38f1152010f38eef983f5ff14e046e
mana <manashe.tsx@gmail.com>
2025-03-21 11:07:14 -0300

    serving files

--- 021_serving-files/02_serving/04_FileServer/03/main.go
+++ 021_serving-files/02_serving/04_FileServer/03/main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", cat)
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func cat(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="/assets/cat.jpg">`)
+	io.WriteString(w, `<img src="/resources/cat.jpg">`)
 }
 
 /*
